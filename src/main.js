af18a6e1a72b2f08b76e3b70a621fd1534b580ed
Rory <60672202+JustAnother-Programmer@users.noreply.github.com>
2022-06-27 19:06:29 +0100

    Complete main functionality

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const noblox = require('noblox.js')
 const { app, BrowserWindow, ipcMain } = require('electron')
 
 let win
@@ -18,6 +19,40 @@ const createWindow = () => {
     win.loadFile(path.join(__dirname, 'public/index.html'))
 }
 
+// Noblox functions
+async function getUserIdFromName(username) {
+    var userId = await noblox.getIdFromUsername(username)
+    .then((res) => { return res })
+    .catch(err => {
+        console.log(err)
+        win.webContents.send('fromMain', {success: false, error: 'Could not find user'})
+    })
+
+    return userId
+}
+
+async function getUserGroups(userId) {
+    var groups = await noblox.getGroups(userId)
+    .then((res) => { return res })
+    .catch(err => {
+        console.log(err)
+        win.webContents.send('fromMain', {success: false, error: 'Could get groups'})
+    })
+
+    return groups
+}
+
+async function getGroupFromId(id) {
+    var group = await noblox.getGroup(id)
+    .then((res) => { return res })
+    .catch(err => {
+        console.log(err)
+        win.webContents.send('fromMain', {success: false, error: 'Could get group from ID'})
+    })
+
+    return group
+}
+
 // Events
 app.whenReady().then(() => {
     createWindow()
@@ -32,9 +67,49 @@ app.on('window-all-closed', () => {
 })
 
 ipcMain.on('toMain', (event, args) => {
-    console.log(`Args: ${args}`)
+    var data = JSON.parse(args)
+    var groupIds = data.ids
+    
+    // Get the userID
+    getUserIdFromName(data.username).then(res => {
+        // Get what groups the user is in
+        getUserGroups(res).then(res2 => {
+            var ids = []
+            var names = []
+            var userInGroupsIds = []
+            var userInGroupsNames = []
+            var notIn = []
 
-    // TODO: Process using roblox api in here and returrn the data to the renderer for display
+            // Collect all of the IDs of the groups the user is in
+            for (let i = 0; i < res2.length; i++) {
+                ids.push(res2.at(i).Id)
+                names.push(res2.at(i).Name)
+            }
 
-    win.webContents.send('fromMain', {success: true, data: 'yes'})
+            // Check through the ids entered in the client side to see if they are in the grabbed IDs
+            for (let i = 0; i < ids.length; i++) {
+                if (groupIds.includes(ids[i].toString())) {
+                    userInGroupsIds.push(ids[i])
+                    userInGroupsNames.push(names[i])
+                }
+            }
+            
+            for (let i = 0; i < groupIds.length; i++) {
+                if (!userInGroupsIds.toString().includes(groupIds[i])) {
+                    getGroupFromId(parseInt(groupIds[i])).then(res3 => {
+                        notIn.push(res3.name)
+                        
+                        // Should probably fix, its kinda dirty as it will send the event with data multiple times to the renderer
+                        // It also massively increases the time but we move
+                        win.webContents.send('fromMain', {success: true, data: {
+                            numChecked: groupIds.length,
+                            userIn: userInGroupsIds.length,
+                            names: userInGroupsNames,
+                            notInGroups: notIn
+                        }})
+                    })
+                }
+            }
+        })
+    })
 })
--- src/public/js/renderer.js
+++ src/public/js/renderer.js
@@ -1,4 +1,5 @@
 const groupAddButton = document.getElementById('groupAdd')
+const groupRemoveButton = document.getElementById('groupRemove')
 const searchButton = document.getElementById('lookupButton')
 
 groupAddButton.addEventListener('click', () => {
@@ -11,19 +12,56 @@ groupAddButton.addEventListener('click', () => {
     div.appendChild(newInput)
 })
 
+groupRemoveButton.addEventListener('click', () => {
+    const div = document.getElementById('groupIds')
+
+    if (div.childElementCount === 1) return
+
+    div.removeChild(div.lastChild)
+})
+
 searchButton.addEventListener('click', () => {
+    const errorText = document.getElementById('errorText')
+    const resultText = document.getElementById('resultText')
+    errorText.innerText = ""
+    resultText.innerText = ""
+
     const groupIds = document.getElementById('groupIds')
+    const username = document.getElementById('username').value
     var ids = []
+
+    if (username === "") {
+        errorText.innerText = "A Username must be provided before conducting a search!"
+        return
+    }
 
     for (let i = 0; i < groupIds.childElementCount; i++) {
         var element = groupIds.children.item(i)
 
+        if (element.value === "") continue
+
         ids.push(element.value)
     }
 
-    window.api.sendToMain(ids)
+    var dataPacket = {
+        username,
+        ids
+    }
+
+    window.api.sendToMain(JSON.stringify(dataPacket))
 
     window.api.recieveFromMain((data, args) => {
-        // TODO: Do stuff here based on the response
+        const resultText = document.getElementById('resultText')
+
+        if (args.success === false) {
+            resultText.innerText = args.error
+            return
+        }
+
+        var data = args.data
+
+        resultText.innerText = `Number of groups searched: ${data.numChecked}\nUser In: ${data.userIn}\nNot in: ${data.notInGroups}`
+
+        console.log(data)
     })
 })
